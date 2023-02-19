52ce08fd64ecd0d173f9c3067413f1b92f87082e
Alin Florin <alin@qoffice.ro>
2023-02-20 01:40:44 +0200

    -

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 import express from "express";
-import { FaceMesh } from "@mediapipe/face_mesh";
-import bodyParser from 'body-parser';
-
+import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.raw({ type: "video/mp4", limit: "250mb" }));
 
-
 app.post(`/api/warp`, async (req, res) => {
-  const videoBuffer = req.body;
-  res.writeHead(200, {
-    "Content-Disposition": "attachment; filename=" + `result.mp4`,
-    "Content-Type": "application/octet-stream",
-  });
-  res.end(videoBuffer);
+  res.send('ok');
 });
 
 app.listen(3000, () => {
